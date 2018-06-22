9035601724da264575a5a55eb2394d889cbac12d
Sergey Bayramov <sergey_bayramov@epam.com>
2018-06-22 13:34:14 +0300

    5: Done HW-5

---
+++ src/main/java/lab/model/Customer.java
@@ -0,0 +1,33 @@
+package lab.model;
+
+public class Customer implements Person {
+    private String name;
+    private boolean broke;
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isBroke() {
+        return broke;
+    }
+
+    public void setBroke(boolean broke) {
+        this.broke = broke;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello(Person person) {
+        //Will be implemented in future, I hope...
+    }
+}
